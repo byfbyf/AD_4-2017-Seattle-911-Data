0a398a2a85f23c4df92ff3aad73b60cf2ec86dee
Yifan Bai <yfbai@uw.edu>
2018-03-07 13:05:20 -0800

    finish

--- ui.R
+++ ui.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(ggplot2)
 
 ui <- fluidPage(
-  h2("2017 Seattle 911 Events Map"),
+  h1("2017 Seattle 911 Calls"),
   tabsetPanel(type = "tabs",
     tabPanel("Intro", htmlOutput("intro")),
     tabPanel("Monthly Average Call", 
@@ -65,7 +65,35 @@ ui <- fluidPage(
                )
              )
     ),
-  tabPanel("Call Types in Different Areas")
+  tabPanel("Call Types in Different Areas",
+           sidebarPanel(
+             
+           selectInput('area', 'districts displayed', c('King' = 'K','George' = 'G',
+                                                        'Edward' = 'E','David' = 'D','Charlie' = 'C',
+                                                        'William' = 'W','Nora' = 'N', 'Queen' = 'Q',
+                                                        'Frank' = 'F', 'Mary' = 'M', 'Sam' = 'S',
+                                                        'Robert' = 'R','Ocean' = 'O', 'Boy' = 'B',
+                                                        'Union' = 'U','John' = 'J','Lincoln' = 'L','99' = '99','Harbor' = 'H')),
+           hr(),
+           helpText('Districts of seattle. Select one and see relevant data in that district in 2017!'),
+           h4('District Map of Seattle'),
+           p('This map shows corresponding district areas of Seattle'),
+           img(src = 'map.jpg')),
+          mainPanel (
+            h3('Call Types in Different Areas'),
+            
+            p('This page investigate incidents that occur most in each district in seattle'),
+            p('Below analyze which types of incidents oocur most frequently in certain area 
+              by showing its frequency and percentage of occurances'),
+            tableOutput('info'),
+            
+            h4('Total frequency'),
+            textOutput('total'),
+            
+            p('The pie chart shows the percentage of each type of accidents happened in this area'),
+            plotOutput('pie')
+            )
+  )
 )
 )
 
