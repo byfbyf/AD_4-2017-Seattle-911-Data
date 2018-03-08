12bdf818fe9032763fafb73f0874c80d63ed89f0
Yining Wang <biyao323@gmail.com>
2018-03-07 23:12:10 -0800

    finial version

--- server.R
+++ server.R
@@ -6,6 +6,7 @@ library(stringr)
 library(lubridate)
 library(plotly)
 library(ggrepel)
+library(zipcode)
 
 data.2017 <-read.csv("./2017_Seattle_911_processed.csv", stringsAsFactors = FALSE)
 
@@ -22,21 +23,26 @@ data.2017 <-  data.2017 %>%
   mutate(At.Scene.Date = as.Date(at.scene[, 1], "%m/%d/%Y"),
          At.Scene.Time = format(strptime(at.scene[, 2], "%I:%M:%S %p"), 
                                 format="%H:%M"))
+
+data("zipcode")
+
+seattle.zipcode <- filter(zipcode, city == "Seattle")
+
 server <- function(input, output) {
   #Intro
   output$intro <- renderUI({
-    
-    text <-paste0("<p>Our project is about the dataset of Seattle's 911
-                  data from year 2017. There are <b>", nrow(data.2017), "</b> calls
-                  in total. And the most common subject of the calls is <b>traffic 
-                  related calls</b>.</p> <p>In our project, we will have three sections:
-                  <li>Part 1 displays the average  daily call numbers regarding various topics 
-                  for different months in the year 2017. </li>
-                  <li> Part 2 display a map where officials responds
-                  to the call at secene using leaflet.</li>
-                  <li>Part 3 shows 
-                  type of calls in different areas.</li> </p>")
-    HTML(text)
+    str1 <- paste("Our project is about the dataset of Seattle's 911
+                  data from year 2017. ")
+    str2 <- paste("There are <b>", nrow(data.2017), "</b> calls
+                  in total. The most common subject of the calls is <b>traffic 
+                  related calls.</b><br/>")
+    str3 <- "<u>In our project, we will have three sections:</u>
+            <li>Part 1 displays the average  daily call numbers regarding various topics 
+                for different months in the year 2017. </li>
+            <li>Part 2 display a map where officials responds
+                to the call at secene using leaflet.</li>
+            <li>Part 3 shows type of calls in different areas.</li>"
+    HTML(paste(str1, str2, str3, sep = '<br/>'))
   })
   
   #Returns Month - Count datafram for selected type of call.
@@ -57,8 +63,15 @@ server <- function(input, output) {
     
   })
   
-  #Shows the average per day of the year and the maxium among the 12 month
   output$text1 <- renderText({
+    text <- "There are many different subjects of calls. Choose one you are interested in 
+    to see how many calls per day in each month regarding this topic."
+    return(text)
+  })
+
+  
+  #Shows the average per day of the year and the maxium among the 12 month
+  output$text2 <- renderText({
     month.data.type <- month.data()
     mean<-format(round(mean(month.data.type$Count.Per.Day), 2), nsmall = 2)
     
@@ -69,6 +82,7 @@ server <- function(input, output) {
            The maxium average calls per day is ", 
            format(round(max, 2), nsmall = 2), " on ",max.month, "." )
   })
+  
   
   #Bar chart and line for the count of average daily calls
   output$plot1 <- renderPlot({
@@ -83,6 +97,7 @@ server <- function(input, output) {
     
   })
   
+  # Conver hour to two digits
   hourTime <- function(hour) {
     if(hour < 10) {
       return(paste0("0", hour))
@@ -100,6 +115,21 @@ server <- function(input, output) {
     return(data.2017)
   })
   
+  zip <- reactive({
+    zipcode <- zipcode %>% 
+      filter(zip == input$zipcode)
+  })
+  
+  output$instruction <- renderUI({
+    str1 <- "You can view 911 police at scene place of any 
+                             date period in 2017 by selecting the time of interest."
+    str2 <- "Sliding the slide bar, you can view 911 police 
+                             at scene events happende in the time period of interest."
+    str3 <- "You can also text a zipcode to add a marker 
+                             at the area of your interest. The default vaule is 98105."
+    HTML(paste(str1, str2, str3, sep = '<br/>'))
+  })
+  
   # Render description
   output$text <- renderText({
     description <- paste0("This map shows officers at sencen for the 911 calls
@@ -111,13 +141,26 @@ server <- function(input, output) {
     return(description)
   })
   
+  
+  greenLeafIcon <- makeIcon(
+    iconUrl = "http://leafletjs.com/examples/custom-icons/leaf-green.png",
+    iconWidth = 38, iconHeight = 95,
+    iconAnchorX = 22, iconAnchorY = 94,
+    shadowUrl = "http://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+    shadowWidth = 50, shadowHeight = 64,
+    shadowAnchorX = 4, shadowAnchorY = 62
+  )
+  
   # Render map
+  # Add markers of at scene location and input zip code loaction
   output$map <- renderLeaflet({
     map <- leaflet() %>%
       addTiles() %>%
       addCircleMarkers(lng=data()$Longitude, lat=data()$Latitude, radius = 5, 
                        color = "black", popup = data()$Event.Clearance.Description,
-                       clusterOptions = markerClusterOptions())
+                       clusterOptions = markerClusterOptions()) %>% 
+      addMarkers(lng=zip()$longitude, lat=zip()$latitude, icon = greenLeafIcon)
+      
   })
   
   
@@ -134,9 +177,6 @@ server <- function(input, output) {
     loc[nr, 'Event.Clearance.Group'] <- 'Other'
     loc[nr, 'frequency'] <- sum(other$frequency)
     loc[nr, 'percentage'] <- sum(other$percentage)
-    # loc[nr + 1, 'Event.Clearance.Group'] <- 'Total'
-    # loc[nr + 1, 'frequency'] <- total
-    # loc[nr + 1, 'percentage'] <- '100%'
     return(loc)
     
   })
@@ -156,17 +196,6 @@ server <- function(input, output) {
   })
   
   output$pie <- renderPlotly({
-    # blank_theme <- theme_minimal()+
-    #   theme(
-    #     axis.title.x = element_blank(),
-    #     axis.title.y = element_blank(),
-    #     panel.border = element_blank(),
-    #     panel.grid=element_blank(),
-    #     axis.ticks = element_blank(),
-    #     plot.title=element_text(size=14, face="bold")
-    #   )
-
-    
      plot_ly(location(), labels = ~Event.Clearance.Group, values = ~frequency, type = 'pie',
                  textposition = 'inside',
                  textinfo = 'label+percent',
@@ -180,7 +209,6 @@ server <- function(input, output) {
       layout(title = 'Percentage of types of incidents in a district',
              xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
              yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
-    
    })
 }
 shinyUI(server)
--- ui.R
+++ ui.R
@@ -5,108 +5,116 @@ library(ggplot2)
 library(plotly)
 
 ui <- fluidPage(
-  h1("2017 Seattle 911 Calls"),
+  theme = "style.css",
+  h1(paste0("2017 Seattle "), span("911"), "Calls"),
+  h4("Info201 AD Win18 Group#4: Yining Wang, Yifan Bai, Coco Ye"),
   tabsetPanel(type = "tabs",
-    tabPanel("Intro", htmlOutput("intro")),
-    tabPanel("Monthly Average Call", 
-             sidebarLayout(
-               sidebarPanel(
-                 selectInput(inputId = "type", label = strong("Type"),
-                   choices = c("All", "Traffic related calls",                
-                               "Disturbances",                         
-                               "Suspicious circumstances",             
-                               "Motor vehicle collision investigation", 
-                               "False alacad",                          
-                               "Trespass",                             
-                               "Liquor violations",     
-                               "Car prowl",                      
-                               "Shoplifting",                          
-                               "Nuisance, mischief",              
-                               "Burglary",                       
-                               "Other property",                       
-                               "Behavioral health",
-                               "Auto thefts", 
-                               "Assaults",                             
-                               "Hazards",                    
-                               "Property damage",                   
-                               "Narcotics complaints",                
-                               "Property - missing, found" ,  
-                               "Fraud calls", "Threats, harassment",                  
-                               "Arrest", "Person down/injury", 
-                               "Robbery",                              
-                               "Miscellaneous misdemeanors",
-                               "Persons - lost, found, missing","Animal complaints" ,                   
-                               "Bike","Weapons calls",
-                               "Lewd conduct" ,                        
-                               "Failure to register (sex offender)" ,  
-                               "Prowler","Harbor calls",                        
-                               "Prostitution","Public gatherings",
-                               "Drive by (no injury)",              
-                               "Other vice","Reckless burning",
-                               "Homicide",                            
-                               "Vice calls"),
-                   selected = "All")               ),
-           mainPanel(
-             p("We thought that the call numbers might vary at different time
-              of the year, so we get the average calls per day regarding different
-               topics in the 12 months."),
-             plotOutput("plot1"),
-             textOutput("text1")
-             
-                            )
-           )
-     ),
-    tabPanel("Where and When are Officers at Scene",
-             sidebarLayout(
-               sidebarPanel(
-                 dateRangeInput("date", "Date in 2017: ", 
-                                start = "2017-1-1",
-                                end = "2017-1-31",
-                                min = "2017-1-1",
-                                max = "2017-12-31",
-                                format = "mm/dd/yy"),
-                 sliderInput("time", "Time Range: ",
-                             0, 24, c(0, 24), 1, 
-                             round = FALSE)
-               ),
-               mainPanel(
-                 h3("2017 Seattle 911 at Scene Events Map"),
-                 textOutput("text"),
-                 leafletOutput("map", width = 1000, height = 600)
-               )
-             )
-    ),
-  tabPanel("Call Types in Different Areas",
-           sidebarPanel(
-             
-           selectInput('area', 'districts displayed', c('King' = 'K','George' = 'G',
-                                                        'Edward' = 'E','David' = 'D','Charlie' = 'C',
-                                                        'William' = 'W','Nora' = 'N', 'Queen' = 'Q',
-                                                        'Frank' = 'F', 'Mary' = 'M', 'Sam' = 'S',
-                                                        'Robert' = 'R','Ocean' = 'O', 'Boy' = 'B',
-                                                        'Union' = 'U','John' = 'J','Lincoln' = 'L','99' = '99','Harbor' = 'H')),
-           hr(),
-           helpText('Districts of seattle. Select one and see relevant data in that district in 2017!'),
-           h4('District Map of Seattle'),
-           p('This map shows corresponding district areas of Seattle'),
-           img(src = 'map.jpg')),
-          mainPanel (
-            h3('Call Types in Different Areas'),
-            
-            p('This page investigate incidents that occur most in each district in seattle'),
-            p('Below analyze which types of incidents oocur most frequently in certain area 
-              by showing its frequency and percentage of occurances'),
-            tableOutput('info'),
-            
-            h4('Total frequency'),
-            textOutput('total'),
-            hr(),
-            plotlyOutput('pie'),
-            p('The pie chart shows the percentage of each type of accidents happened in this area')
-            
-            )
+              tabPanel("Intro", htmlOutput("intro"),
+                       img(id = "img",src = "911.jpg", alt = "img")),
+              tabPanel("Monthly Average Call", 
+                       sidebarLayout(
+                         sidebarPanel(
+                           selectInput(inputId = "type", label = strong("Type"),
+                                       choices = c("All", "Traffic related calls",                
+                                                   "Disturbances",                         
+                                                   "Suspicious circumstances",             
+                                                   "Motor vehicle collision investigation", 
+                                                   "False alacad",                          
+                                                   "Trespass",                             
+                                                   "Liquor violations",     
+                                                   "Car prowl",                      
+                                                   "Shoplifting",                          
+                                                   "Nuisance, mischief",              
+                                                   "Burglary",                       
+                                                   "Other property",                       
+                                                   "Behavioral health",
+                                                   "Auto thefts", 
+                                                   "Assaults",                             
+                                                   "Hazards",                    
+                                                   "Property damage",                   
+                                                   "Narcotics complaints",                
+                                                   "Property - missing, found" ,  
+                                                   "Fraud calls", "Threats, harassment",                  
+                                                   "Arrest", "Person down/injury", 
+                                                   "Robbery",                              
+                                                   "Miscellaneous misdemeanors",
+                                                   "Persons - lost, found, missing","Animal complaints" ,                   
+                                                   "Bike","Weapons calls",
+                                                   "Lewd conduct" ,                        
+                                                   "Failure to register (sex offender)" ,  
+                                                   "Prowler","Harbor calls",                        
+                                                   "Prostitution","Public gatherings",
+                                                   "Drive by (no injury)",              
+                                                   "Other vice","Reckless burning",
+                                                   "Homicide",                            
+                                                   "Vice calls"),
+                                       selected = "All"),
+                           textOutput("text1")
+                          ),
+                         
+                         mainPanel(
+                           p(class = "text", "We thought that the call numbers might vary at different time
+                             of the year, so we get the average calls per day regarding different
+                             topics in the 12 months."),
+                           textOutput("text2"),
+                           plotOutput("plot1")
+                           )
+                         )
+              ),
+              tabPanel("Time and places of Officers at Scene",
+                       sidebarLayout(
+                         sidebarPanel(
+                           dateRangeInput("date", "Date in 2017: ", 
+                                          start = "2017-1-1",
+                                          end = "2017-1-31",
+                                          min = "2017-1-1",
+                                          max = "2017-12-31",
+                                          format = "mm/dd/yy"),
+                           sliderInput("time", "Time Range: ",
+                                       0, 24, c(0, 24), 1, 
+                                       round = FALSE),
+                           numericInput("zipcode", "Zipcode in Seattle:", 
+                                        value = 98105, min = 98060, max = 98199),
+                           htmlOutput("instruction")
+                         ),
+                         mainPanel(
+                           h2("2017 Seattle 911 at Scene Events Map"),
+                           textOutput("text"),
+                           leafletOutput("map", width = 1000, height = 600)
+                         )
+                       )
+              ),
+              tabPanel("Call Types in Different Areas",
+                       sidebarPanel(
+                         
+                         selectInput('area', 'districts displayed', c('King' = 'K','George' = 'G',
+                                                                      'Edward' = 'E','David' = 'D','Charlie' = 'C',
+                                                                      'William' = 'W','Nora' = 'N', 'Queen' = 'Q',
+                                                                      'Frank' = 'F', 'Mary' = 'M', 'Sam' = 'S',
+                                                                      'Robert' = 'R','Ocean' = 'O', 'Boy' = 'B',
+                                                                      'Union' = 'U','John' = 'J','Lincoln' = 'L','99' = '99','Harbor' = 'H')),
+                         hr(),
+                         helpText(class = "text", 'Districts of seattle. Select one and see relevant data in that district in 2017!'),
+                         h3('District Map of Seattle'),
+                         p(class = "text", 'This map shows corresponding district areas of Seattle'),
+                         img(src = 'map.jpg')),
+                       mainPanel (
+                         h2('Call Types in Different Areas'),
+                         
+                         p(class = "text", 'This page investigate incidents that occur most in each district in seattle'),
+                         p(class = "text", 'Below analyze which types of incidents oocur most frequently in certain area 
+                           by showing its frequency and percentage of occurances'),
+                         tableOutput('info'),
+                         
+                         h3('Total frequency'),
+                         textOutput('total'),
+                         hr(),
+                         p(class = "text", 'The pie chart shows the percentage of 
+                           each type of accidents happened in this area'),
+                         plotlyOutput('pie')
+                       )
+              )
   )
-)
-)
+  )
 
 shinyUI(ui)
